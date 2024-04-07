a11131d3cc0c0bd49d39ef5a1a249a0607831563
Elizabethcr05 <162637780+Elizabethcr05@users.noreply.github.com>
2024-04-06 23:33:38 -0500

    Add files via upload

---
+++ JAVA/AccesModifiers/Bank/Bankk.java
@@ -0,0 +1,19 @@
+package bankk;
+public class Bankk{
+  private CheckingAccount accountOne;
+  private CheckingAccount accountTwo;
+
+  public Bankk(){
+    accountOne = new CheckingAccount("Zeus", 100, "1");
+    accountTwo = new CheckingAccount("Hades", 200, "2");
+  }
+
+  public static void main(String[] args){
+    Bankk bankOfGods = new Bankk();
+    System.out.println(bankOfGods.accountOne.getBalance());
+    bankOfGods.accountOne.setBalance(5000);
+    System.out.println(bankOfGods.accountOne.getBalance());
+    System.out.println(bankOfGods.accountOne.getMonthlyInterest());
+
+  }
+}
