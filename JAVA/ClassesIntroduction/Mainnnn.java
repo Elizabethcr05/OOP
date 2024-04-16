1d12bde9f78b32141d78f805e752d3b4a7e6d33c
Elizabethcr05 <162637780+Elizabethcr05@users.noreply.github.com>
2024-04-16 17:09:33 -0500

    Add files via upload

---
+++ JAVA/ClassesIntroduction/Mainnnn.java
@@ -0,0 +1,11 @@
+package mainnnn;
+
+public class Mainnnn {
+    int x=5;
+
+    public static void main(String[] args) {
+    Mainnnn myObj = new Mainnnn();
+      System.out.println(myObj.x);
+    }
+    
+}
