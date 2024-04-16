1fb22623d30a825497a40d9e0f47f25fe6f5d1b7
Elizabethcr05 <162637780+Elizabethcr05@users.noreply.github.com>
2024-04-16 17:11:50 -0500

    Add files via upload

---
+++ JAVA/ClassesIntroduction/Main2.java
@@ -0,0 +1,14 @@
+package main2;
+
+public class Main2 {
+
+int x = 5;
+  
+    public static void main(String[] args) {
+      Main2 myObj1 = new Main2();  // Object 1
+      Main2 myObj2 = new Main2();  // Object 2
+      System.out.println(myObj1.x);
+      System.out.println(myObj2.x);
+
+  }
+}
